d6f3fa1e4f534bbdb328a63de571c2aac5d3e217
AkshinSamed <samedliakshin@gmail.com>
2023-04-05 21:47:18 +0400

    StudentService interface and StudentServiceImplement class created in service package

---
+++ src/main/java/com/sms/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.sms.service;
+
+import com.sms.entity.Student;
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+}
---
+++ src/main/java/com/sms/service/implement/StudentServiceImplement.java
@@ -0,0 +1,24 @@
+package com.sms.service.implement;
+
+import com.sms.entity.Student;
+import com.sms.repository.StudentRepository;
+import com.sms.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImplement implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    //Autowiring with constructor
+    public StudentServiceImplement(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
